9ecf604727487743cb3ddd3e234f063b4db89fbb
NateClarkTech <NateClarkTech@gmail.com>
2025-10-20 16:48:09 -0400

    new branch

--- main.py
+++ main.py
@@ -21,11 +21,11 @@ N_REL_CLASSES: Specified number of relevant classes
 # DATA_SOURCE = "MNIST" # NOTE: currently multiplied by 10x to get ~130,000 samples
 # N_REL_CLASSES = 4
 
-# DATA_SOURCE = "MNIST_2D"
-# N_REL_CLASSES = 4
+DATA_SOURCE = "MNIST_2D"
+N_REL_CLASSES = 4
 
-DATA_SOURCE = "EMNIST"
-N_REL_CLASSES = 3
+# DATA_SOURCE = "EMNIST"
+# N_REL_CLASSES = 3
 
 # DATA_SOURCE = "NICE"
 # N_REL_CLASSES = 4
@@ -37,7 +37,7 @@ N_REL_CLASSES = 3
 KAPPA: Paranoia Parameter
 (single value for now)
 '''
-KAPPA = 0.1
+KAPPA = 2 #0.5, , 1.4, 10
 # # KAPPAS = [0.5] #0.5, , 1.4, 10
 # |- Array of Kappas to run ARED on
 # |- Run more than one for graphing purposes
@@ -97,14 +97,14 @@ window_size: size of the data_window window saved by ARED
 |- int: larger window size means it remembers more data
 |- WARNING: value must be larger than 0
 '''
-DATA_WINDOW_SIZE = 10000 # ultimately needs to be driven by anomaly ratio
+DATA_WINDOW_SIZE = 100 # ultimately needs to be driven by anomaly ratio
 
 '''
 NUM_POINTS_TO_PROCESS: Number of points in dataset to process
 |- -1: process all the data
 |-  0 to inf: process up to that number if data is available
 '''
-NUM_POINTS_TO_PROCESS = 10000#-1
+NUM_POINTS_TO_PROCESS = -1
 
 # '''
 # NUM_RUN_TO_AVE: number of runs to average.
@@ -116,7 +116,7 @@ NUM_POINTS_TO_PROCESS = 10000#-1
 '''
 GRAPH_BATCH_SIZE: number of points in batch for stats purposes.
 '''
-GRAPH_BATCH_SIZE = 500
+GRAPH_BATCH_SIZE = 1000
 
 '''
 VERBOSE_FLAGS: Array of control flags to make ARED loud or quiet
